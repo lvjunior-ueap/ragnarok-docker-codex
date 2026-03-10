74cc9906104daa0e6a1e126ad153097b05affa22
guiapq <guiapq@gmail.com>
2026-03-10 16:21:57 -0300

    melhorias linux

--- docker/rathena/start.sh
+++ docker/rathena/start.sh
@@ -5,16 +5,20 @@ echo "Configurando conexão com banco..."
 mkdir -p /opt/rathena/conf/import
 
 cat <<EOF > /opt/rathena/conf/import/inter_conf.txt
-login_server_ip: ${RATHENA_DB_HOST}
-login_server_port: ${RATHENA_DB_PORT}
-login_server_id: ${RATHENA_DB_USER}
-login_server_pw: ${RATHENA_DB_PASS}
-login_server_db: ${RATHENA_DB_NAME}
+login_server_ip: db
+login_server_port: 3306
+login_server_id: ${MYSQL_USER}
+login_server_pw: ${MYSQL_PASSWORD}
+login_server_db: ${MYSQL_DATABASE}
 EOF
 
 echo "Aguardando MariaDB iniciar..."
 
-sleep 10
+until mysql -h db -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "SELECT 1"; do
+  echo "MariaDB ainda não respondeu..."
+  sleep 2
+done
+
 
 cd /opt/rathena
 
